a41ce5dd57f51296b4faf589119c527b3a01fb0c
SauceX22 <mahmoudzak4@gmail.com>
2025-03-15 00:51:23 +0300

    use client for whole page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import { AnimatedBackground } from "@/components/animated-background"
-import { Navbar } from "@/components/navbar"
-import { HeroSection } from "@/components/hero-section"
-import { AboutSection } from "@/components/about-section"
-import { StructureSection } from "@/components/structure-section"
-import { PrizesSection } from "@/components/prizes-section"
-import { RegistrationSection } from "@/components/registration-section"
-import { ContactSection } from "@/components/contact-section"
-import { Footer } from "@/components/footer"
+"use client";
+
+import { AnimatedBackground } from "@/components/animated-background";
+import { Navbar } from "@/components/navbar";
+import { HeroSection } from "@/components/hero-section";
+import { AboutSection } from "@/components/about-section";
+import { StructureSection } from "@/components/structure-section";
+import { PrizesSection } from "@/components/prizes-section";
+import { RegistrationSection } from "@/components/registration-section";
+import { ContactSection } from "@/components/contact-section";
+import { Footer } from "@/components/footer";
 
 export default function Home() {
   return (
@@ -21,6 +23,5 @@ export default function Home() {
       <ContactSection />
       <Footer />
     </main>
-  )
+  );
 }
-
